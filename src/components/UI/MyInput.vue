b3787f16aba822b73641bbc7637d356c306b9784
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2025-06-19 21:08:15 +0700

    добавлен компонент input

---
+++ src/components/UI/MyInput.vue
@@ -0,0 +1,33 @@
+<template>
+    <input type="text" class="input" />
+</template>
+
+<script>
+export default {
+    name: 'my-input',
+}
+</script>
+
+<style lang="less">
+.input {
+    box-shadow: none;
+    outline: none;
+    color: @text-primary;
+    max-width: 464px;
+    width: 100%;
+    padding: 12px 8px 12px 12px;
+    border: 1px solid @border-light;
+    background-color: transparent;
+    border-radius: 8px;
+
+    &::placeholder {
+        color: @text-tertiary;
+    }
+
+    &:focus {
+        &::placeholder {
+            color: transparent;
+        }
+    }
+}
+</style>
