c42f5c9d7df81c7e33f3d9e1ed3feeb63411c450
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2025-06-19 16:35:19 +0700

    добавлен компонент кнопки

---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <button class="button">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'my-button',
+}
+</script>
+
+<style lang="less">
+.button{
+    display: flex;
+    padding: 16px 32px;
+    gap: 8px;
+    justify-content: center;
+    align-items: center;
+    border-radius: 16px;
+    border: none;
+    background-color: transparent;
+    outline: none;
+    text-decoration: none;
+    font-family: @font1;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 1.5;
+    text-align: center;
+    transition: all 0.3s;
+    overflow: hidden;
+    cursor: pointer;
+}
+</style>
