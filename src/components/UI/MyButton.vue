aca12d6b6c24ae993105833b813356326a070293
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2025-06-19 17:23:42 +0700

    Изменен компонент кнопки

--- src/components/UI/MyButton.vue
+++ src/components/UI/MyButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="button">
+    <button class="button" :class="buttonClasses">
         <slot></slot>
     </button>
 </template>
@@ -7,6 +7,28 @@
 <script>
 export default {
     name: 'my-button',
+    props: {
+        buttonColor:{ 
+            type: Boolean,
+        },
+        buttonBorder: {
+            type: Boolean,
+        },
+        buttonDisabled: {
+            type: Boolean,
+            default: false
+        }
+    },
+    computed: {
+        buttonClasses(){
+            return{
+                'button__color': this.buttonColor,
+                'button__border': this.buttonBorder,
+                'button__color--disabled': this.buttonColor && this.buttonDisabled,
+                'button__border--disabled': this.buttonBorder && this.buttonDisabled
+            }
+        }
+    }
 }
 </script>
 
@@ -30,5 +52,45 @@ export default {
     transition: all 0.3s;
     overflow: hidden;
     cursor: pointer;
+
+    &__color{
+        background-color: @bg-brand;
+        color: @text-invert;
+
+        &:hover{
+            @media(hover: hover){
+                background-color: @bg-hover;
+            }
+        }
+
+        &:active{
+            background-color: @bg-hover;
+        }
+
+        &--disabled{
+            background-color: @bg-extraLight;
+            pointer-events: none;
+        }
+    }
+
+    &__border{
+        border: 1px solid @border-light;
+
+        &:hover{
+            @media(hover: hover){
+                background-color: @bg-tertiary;
+            }
+        }
+
+        &:active{
+            background-color: @bg-tertiary;
+        }
+
+        &--disabled{
+            border-color: @border-extralight;
+            color: @text-tertiary;
+            pointer-events: none;
+        }
+    }
 }
 </style>
