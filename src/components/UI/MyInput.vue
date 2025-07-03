9f56100d2231f5e59d18281a2677660236db258c
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2025-07-03 13:49:44 +0700

    Добавлено двусторонее связывание для input

--- src/components/UI/MyInput.vue
+++ src/components/UI/MyInput.vue
@@ -1,10 +1,18 @@
 <template>
-    <input type="text" class="input paragraph-16-regular" />
+    <input type="text" class="input paragraph-16-regular" :value="modelValue" @input="updateInput"/>
 </template>
 
 <script>
 export default {
     name: 'my-input',
+    props: {
+        modelValue: [String, Number],
+    },
+    methods: {
+        updateInput(event) {
+            this.$emit('update:modelValue', event.target.value)
+        },
+    },
 }
 </script>
 
