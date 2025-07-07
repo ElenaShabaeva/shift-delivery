0724289d7b7b3def030decd1b4ff5f2e1adcb712
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2025-07-07 18:57:51 +0700

    инициализация новых store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
 import { createStore } from "vuex"
 import { auth } from "./auth.module"
+import { delivery } from "./delivery.module"
+import { user } from "./user.module"
 
 export default createStore({
     modules: {
-        auth: auth
+        auth: auth,
+        delivery: delivery,
+        user: user,
     }
 })
