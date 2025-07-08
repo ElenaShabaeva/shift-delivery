a2c3c948a8eec0d8f079f3a761144f20141edf44
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2025-07-08 17:55:01 +0700

    добавлен компонент modal-window (для уведомлений)

---
+++ src/components/UI/MyModalWindow.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="modal-window">
+        <div class="modal-window__container">
+            <slot name="content"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'my-modal-window',
+}
+</script>
+
+<style lang="less">
+.modal-window {
+    background-color: fade(@text-primary, 85%);
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 110;
+
+    &__container {
+        margin: 230px auto;
+        border-radius: 24px;
+        background-color: @white;
+        max-width: 544px;
+        width: 100%;
+    }
+}
+</style>
